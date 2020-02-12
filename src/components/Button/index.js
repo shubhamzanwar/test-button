10870dec0fdb8a03eb992d636190a504634e3181
sarvanideekshitula <sarvanimini@gmail.com>
2020-02-12 14:30:39 +0530

    Added type of the button with testcases

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export const Button = ({children, testID, onClick}) => (
-    <button data-testid={testID} onClick={() => onClick()}>
-        {children}
+const Button = ({
+  children, testID, onClick, type,
+}) => {
+  const style = type === 'rounded' ? { borderRadius: '50%' } : {};
+  return (
+
+    <button style={style} data-testid={testID} onClick={() => onClick()}>
+      {children}
     </button>
-);
+
+  );
+};
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  testID: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired,
+};
+export default Button;
--- src/components/Button/index.test.js
+++ src/components/Button/index.test.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import {Button} from './index';
 import { render, fireEvent } from '@testing-library/react';
+import Button from './index';
 
 describe('the button component', () => {
-    it('should render correctly', () => {
-        const { asFragment } = render(<Button testID="test-btn">Test text</Button>);
+  it('should render correctly', () => {
+    const { asFragment } = render(<Button testID="test-btn">Test text</Button>);
 
-        expect(asFragment()).toMatchSnapshot();
-    });
+    expect(asFragment()).toMatchSnapshot();
+  });
 
-    it('should call the onClick prop when the button is clicked', () => {
-        const onClickMock = jest.fn();
-        const { getByTestId } = render(<Button testID="test-btn" onClick={onClickMock}>Test text</Button>);
+  it('should call the onClick prop when the button is clicked', () => {
+    const onClickMock = jest.fn();
+    const { getByTestId } = render(<Button testID="test-btn" onClick={onClickMock}>Test text</Button>);
 
-        fireEvent.click(getByTestId("test-btn"));
-        expect(onClickMock).toHaveBeenCalledWith();
-    });
-})
+    fireEvent.click(getByTestId('test-btn'));
+    expect(onClickMock).toHaveBeenCalledWith();
+  });
+  it('should render correctly as round button', () => {
+    const { asFragment } = render(<Button testID="test-btn" type="rounded">Test text</Button>);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it('should render correctly as rectangle button', () => {
+    const { asFragment } = render(<Button testID="test-btn" type="rect">Test text</Button>);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
